8feb042fa0023129fa7fa8bdad368fccc090b24d
Martín Gárate <martingarate100@gmail.com>
2025-12-17 17:29:51 -0300

    vava

--- MovilApp/Converters/FeedbackStatusConverter.cs
+++ MovilApp/Converters/FeedbackStatusConverter.cs
@@ -13,10 +13,7 @@ namespace MovilApp.Converters
             return Color.FromArgb("#6b7280");
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
     }
 
     public class FeedbackTextConverter : IValueConverter
@@ -25,54 +22,31 @@ namespace MovilApp.Converters
         {
             if (value is bool feedbackRecibido)
             {
-                return feedbackRecibido ? "? Recibido" : "? Pendiente";
+                // CORRECCIÓN: Se eliminaron los signos '?'
+                return feedbackRecibido ? "Recibido" : "Pendiente";
             }
-            return "? Sin Estado";
+            return "Sin Estado";
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
     }
 
+    // El resto de conversores se mantienen igual pero sin arrojar errores...
     public class StringNotEmptyConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return value is string str && !string.IsNullOrEmpty(str);
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is string str && !string.IsNullOrEmpty(str);
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
     }
 
     public class StringNotNullConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return value is string str && !string.IsNullOrWhiteSpace(str);
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is string str && !string.IsNullOrWhiteSpace(str);
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
     }
 
-    // Nuevo converter para invertir booleano
     public class InverseBoolConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return value is bool boolValue && !boolValue;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return value is bool boolValue && !boolValue;
-        }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is bool boolValue && !boolValue;
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => value is bool boolValue && !boolValue;
     }
 }
